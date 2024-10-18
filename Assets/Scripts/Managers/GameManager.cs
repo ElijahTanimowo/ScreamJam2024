403b32c0f1b112d81dd7ce5c5c97bfb893b48858
RalphRobes0324 <ralphrobes123@gmail.com>
2024-10-18 08:04:31 -0400

    Created the base of GameManager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private Transform player;
+
+    private void Awake()
+    {
+        //Game Manager in world, destory
+        if (!instance)
+        {
+            Destroy(instance.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
