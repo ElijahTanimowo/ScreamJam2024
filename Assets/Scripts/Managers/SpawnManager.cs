50b833cc8e2d38e534ffbf33bb87ed47c34f8521
RalphRobes0324 <ralphrobes123@gmail.com>
2024-10-19 09:14:46 -0400

    Set up Enemy spawn
    - Checks if spawn can spawn certain areas

---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static SpawnManager instance;
+
+    [SerializeField] GameObject[] monsters;
+    [SerializeField] BoxCollider2D spawnAreaCollider;
+    [SerializeField] Vector2 spawnCheckSize = new Vector2(1, 1);
+    [SerializeField] int maxAttemptToSpawn = 3;
+
+    private void Awake()
+    {
+        //Game Manager in world, destory
+        if (instance != null)
+        {
+            Destroy(instance.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public void canSpawn()
+    {
+
+    }
+
+    /// <summary>
+    /// Spawn Enemies
+    /// </summary>
+    void SpawnEnemy()
+    {
+        
+        Vector2 randomSpawnPoint = Vector2.zero;
+        bool spawnPosFound = false;
+
+        //Get the size of the spawn
+        Bounds spawnBound = spawnAreaCollider.bounds;
+
+        //Keep trying to spawn monster
+        for (int i = 0; i < maxAttemptToSpawn; i++) 
+        {
+            //Get Random spawn
+            randomSpawnPoint = new Vector2(
+                Random.Range(spawnBound.min.x, spawnBound.max.x),
+                Random.Range(spawnBound.min.y, spawnBound.max.y)
+                );
+
+            //Check can spawn in area
+            if (CheckCanSpawn(randomSpawnPoint))
+            {
+                spawnPosFound = true;
+                break;
+            }
+        }
+
+        //Check can spawn
+        if (spawnPosFound) 
+        {
+            int index = Random.Range(0, monsters.Length);
+            Instantiate(monsters[index], randomSpawnPoint, Quaternion.identity);
+        }
+
+    }
+
+    /// <summary>
+    /// Check monster can spawn in coords
+    /// </summary>
+    /// <param name="randomSpawnPoint"></param>
+    /// <returns></returns>
+    private bool CheckCanSpawn(Vector2 randomSpawnPoint)
+    {
+        //get all positions in box
+        Collider2D[] spawnOpen = Physics2D.OverlapBoxAll(randomSpawnPoint, spawnCheckSize, 0f);
+
+        //Check spawn point contains Obstacle
+        foreach (Collider2D pos in spawnOpen)
+        {
+            if (pos.CompareTag("Obstacle"))
+            {
+                //Cannot spawn
+                return false;
+            }
+        }
+        //Can Spawn
+        return true;
+    }
+}
