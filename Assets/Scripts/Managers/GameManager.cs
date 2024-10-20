7c6d4a9e97db668bd172df455d30a7b33a365ba3
RalphRobes0324 <ralphrobes123@gmail.com>
2024-10-19 20:46:48 -0400

    Added a Timer

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour
     private Transform player;
     [SerializeField] float spawnCooldown = 5f;
     [SerializeField] bool onCooldown = false;
+    [SerializeField] float timeRemaining = 180f;
+    [SerializeField] bool isTimeRunning = false;
 
     private void Awake()
     {
@@ -33,14 +35,21 @@ public class GameManager : MonoBehaviour
         {
             player = PlayerManager.instance.player.transform;
         }
+
+        isTimeRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         StartCoroutine(SpawnMonstersTimer());
+        Timer();
     }
 
+    /// <summary>
+    /// Controls the time when monster spawns
+    /// </summary>
+    /// <returns></returns>
     IEnumerator SpawnMonstersTimer()
     {
         while (true)
@@ -56,8 +65,32 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Spawns Monsters in world
+    /// </summary>
     void SpawnMonsters()
     {
         SpawnManager.instance.CanSpawn(player);
+    }
+
+    /// <summary>
+    /// Controls the time of the game
+    /// </summary>
+    void Timer()
+    {
+        if (isTimeRunning)
+        {
+            if(timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                Debug.Log(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time is up");
+                timeRemaining = 0;
+                isTimeRunning = false;
+            }
+        }
     }
 }
