276df98be806f85d8ff1076e77a67055668d237c
ElijahTanimowo <elitanimowo@gmail.com>
2024-10-20 20:30:00 -0400

    Basic Main Menu
    Set up the basic main menu

--- Assets/Scripts/Menus/Main Menu/MainMenu.cs
+++ Assets/Scripts/Menus/Main Menu/MainMenu.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public LevelLoader levelLoader;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        levelLoader.LoadGame();
     }
 
     public void OpenOptions()
