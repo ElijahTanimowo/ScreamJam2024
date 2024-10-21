b3b275d92fd35eb54afb44a9feb71e8641b49046
ElijahTanimowo <elitanimowo@gmail.com>
2024-10-21 00:52:21 -0400

    Added Title
    added  title and changed from options to nightmares menu

--- Assets/Scripts/Menus/Nightmares Menu/NightmaresMenu.cs
+++ Assets/Scripts/Menus/Nightmares Menu/NightmaresMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OptionsMenu : MonoBehaviour
+public class NightmaresMenu : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject optionsMenu;
